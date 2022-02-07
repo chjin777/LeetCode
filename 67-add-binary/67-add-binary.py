1f06accef435d2d23f63466aebe28cd578ecc963
Chaejin Park <chjin777@kaist.ac.kr>
2022-02-07 23:52:21 +0900

    Time: 28 ms (96.05%), Space: 14.1 MB (85.07%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,5 +1,5 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         ret = int('0b'+a, 2) + int('0b'+b, 2)
-        return format(ret, 'b')
+        return format(int('0b'+a, 2) + int('0b'+b, 2), 'b')
         
