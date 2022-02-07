592ebd15b845c900e1fab35cb2f1153a22e9cf49
Chaejin Park <chjin777@kaist.ac.kr>
2022-02-08 00:05:34 +0900

    Time: 64 ms (37.76%), Space: 13.9 MB (83.69%) - LeetHub

---
+++ 69-sqrtx/69-sqrtx.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        start = 0
+        end = x
+        while(end>=start):
+            mid = (start + end) // 2
+            if(mid * mid == x):
+                return mid
+            if(mid * mid < x):
+                start = mid + 1
+            else:
+                end = mid - 1    
+        return end
+    ##binary search
+        
